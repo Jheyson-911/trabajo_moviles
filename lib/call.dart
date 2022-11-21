0e3d7c9ddc392e5f40a40577392e22873a372313
jheyson-911 <gompanchiro.911@gmail.com>
2022-11-21 08:32:14 -0500

    finalizacion del proyecto de sms y llamada :D

---
+++ lib/call.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
+import 'package:trabajo_moviles/main.dart';
+
+void main() => runApp(const Call());
+
+class Call extends StatefulWidget {
+  const Call({Key? key}) : super(key: key);
+
+  @override
+  _CallState createState() => _CallState();
+}
+
+class _CallState extends State<Call> {
+  final TextEditingController _numberCtrl = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _numberCtrl.text = "085921191121";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _numberCtrl,
+                decoration: const InputDecoration(labelText: "Phone Number"),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            ElevatedButton(
+              child: const Text("Test Call"),
+              onPressed: () async {
+                FlutterPhoneDirectCaller.callNumber(_numberCtrl.text);
+              },
+            )
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const MyApp()));
+          },
+          tooltip: 'Volver',
+          child: const Icon(Icons.arrow_back),
+        ),
+      ),
+    );
+  }
+}
